60a3e4ec0a72b8cf134515fc0d13a73e33892713
Raihan Adam <hallwack.id@gmail.com>
2026-05-27 22:00:42 +0700

    feat: add obsidian and markdown render plugins

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+	{
+		src = "https://github.com/obsidian-nvim/obsidian.nvim",
+		version = vim.version.range("*"),
+	},
+})
+
+require("obsidian").setup({
+  legacy_commands = false,
+	picker = {
+		name = "snacks.picker",
+	},
+	workspaces = {
+		{ name = "Obsidian Vault", path = "~/Documents/Obsidian/Obsidian Vault" },
+	},
+})
